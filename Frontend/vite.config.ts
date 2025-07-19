1ba096e0ebaeecb3f50e8297dd502adf3d8554db
Krishnamoorthy <dkrish7182006@gmail.com>
2025-07-19 10:59:58 +0530

    routes.json file added

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'routes.json',
+          dest: '.' // copies to dist/routes.json
+        }
+      ]
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
